a55f1552c7dc24a23eb50a8681505b5d58620248
walsand987 <walsand987@gmail.com>
2023-05-24 18:38:58 +0200

    added functionality ot MainActivity.java

--- app/src/main/java/com/example/project_assignment_b22jonan/MainActivity.java
+++ app/src/main/java/com/example/project_assignment_b22jonan/MainActivity.java
@@ -1,19 +1,55 @@
 package com.example.project_assignment_b22jonan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "example";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<Pokemon>>(){}.getType();
+        List<Pokemon> pokemonList = gson.fromJson(json, listType);
+
+        for (Pokemon pokemon : pokemonList) {
+            Log.d("YAAHHHH", pokemon.toString());
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        PokemonAdapter adapter = new PokemonAdapter(this, pokemonList);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.notifyDataSetChanged();
 
     }
 }

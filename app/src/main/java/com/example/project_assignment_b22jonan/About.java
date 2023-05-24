1284796f075e50116dec5bd88aa78309beecf8be
walsand987 <walsand987@gmail.com>
2023-05-24 16:22:38 +0200

    made about activity

---
+++ app/src/main/java/com/example/project_assignment_b22jonan/About.java
@@ -0,0 +1,13 @@
+package com.example.project_assignment_b22jonan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
